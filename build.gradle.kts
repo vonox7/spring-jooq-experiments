c37bc5a6ea62d1202f15df9dc652e7b6beb79d45
Valentin Slawicek <valentin@slawicek.com>
2025-02-23 11:45:54 +0100

    Generate db classes with jooqCodegen gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
 	kotlin("plugin.spring") version "1.9.25"
 	id("org.springframework.boot") version "3.4.3"
 	id("io.spring.dependency-management") version "1.1.7"
+	id("org.jooq.jooq-codegen-gradle") version "3.20.1"
 }
 
 group = "com.slawicek"
@@ -27,6 +28,7 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	jooqCodegen("org.postgresql:postgresql")
 }
 
 kotlin {
@@ -37,4 +39,25 @@ kotlin {
 
 tasks.withType<Test> {
 	useJUnitPlatform()
+}
+
+jooq {
+	configuration {
+		jdbc {
+			driver = "org.postgresql.Driver"
+			url = "jdbc:postgresql://localhost:5432/spring_jooq_experiments"
+			user = "postgres"
+			password = "postgres"
+		}
+		generator {
+			name = "org.jooq.codegen.KotlinGenerator"
+			database {
+				inputSchema = "public"
+			}
+			target {
+				packageName = "com.slawicek.spring_jooq_experiments.db"
+				directory = "src/main/kotlin"
+			}
+		}
+	}
 }
