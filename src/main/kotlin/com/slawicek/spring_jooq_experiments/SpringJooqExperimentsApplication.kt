35decb9aa7e6475bc56384335296adb6f9090ab6
Valentin Slawicek <valentin@slawicek.com>
2025-02-23 11:01:03 +0100

    Project setup with start.spring.io

---
+++ src/main/kotlin/com/slawicek/spring_jooq_experiments/SpringJooqExperimentsApplication.kt
@@ -0,0 +1,11 @@
+package com.slawicek.spring_jooq_experiments
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringJooqExperimentsApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringJooqExperimentsApplication>(*args)
+}
